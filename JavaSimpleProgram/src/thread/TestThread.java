89278a8cb70d0e191e76daa95fa29aa25d658016
Zahid <zahid.aiub6@gmail.com>
2019-10-28 23:09:50 +0600

    add gitignore file

--- JavaSimpleProgram/src/thread/TestThread.java
+++ JavaSimpleProgram/src/thread/TestThread.java
@@ -10,7 +10,7 @@ class Hi extends Thread {
 
 	public void run() {
 		for (int i = 0; i < 6; i++) {
-			System.out.println("Hi...");
+			System.out.println("Hi");
 			try {
 				Thread.sleep(500);
 			} catch (InterruptedException e) {
@@ -24,7 +24,7 @@ class Hello extends Thread {
 
 	public void run() {
 		for (int i = 0; i < 6; i++) {
-			System.out.println("Hello...");
+			System.out.println("Hello");
 			try {
 				Thread.sleep(500);
 			} catch (InterruptedException e) {
